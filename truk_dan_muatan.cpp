dd83ea20b3f6f06a4771dfe5c09f3dda8b2a77e2
kyo21 <kussa.laksana@gmail.com>
2024-01-16 00:25:44 +0800

    adding instruction and file cpp
    adding instruction and file cpp

---
+++ truk_dan_muatan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <set>
+
+using namespace std;
+
+//----kode di atas batas ini tidak boleh diganti atau ditambah----
+
+map<int, map<int, int>> truk;
+
+void bobot_maks(map<int, map<int, int>> truk){
+
+//CODE
+}
+
+
+int main(){
+
+return 0;
+}
