b0f6bce10e07c52a8f4bf28d7f3d0951ff7cf521
Arisujana <odenessa39@gmail.com>
2024-01-16 22:00:11 +0700

    saya menambahkan code di bagian int main ()
    selamat malam pak saya I Dewa Ketut Ari Sujana Putra saya menambhkan code di bagian int main() mohon di atensi

--- truk_dan_muatan.cpp
+++ truk_dan_muatan.cpp
@@ -10,12 +10,40 @@ using namespace std;
 map<int, map<int, int>> truk;
 
 void bobot_maks(map<int, map<int, int>> truk){
+ int maks_bobot = -1; // Inisialisasi maks_bobot dengan nilai yang tidak mungkin
+    pair<int, int> posisi_maks;
+
+    // Menggunakan iterator untuk mengakses elemen map
+    for (auto it1 = truk.begin(); it1 != truk.end(); ++it1) {
+        for (auto it2 = it1->second.begin(); it2 != it1->second.end(); ++it2) {
+            // Melakukan sesuatu dengan nilai bobot pada posisi (it1->first, it2->first)
+            int bobot = it2->second;
+            // Contoh: Menampilkan bobot
+            cout << "Bobot pada posisi (" << it1->first << ", " << it2->first << "): " << bobot << endl;
+
+            // Memeriksa apakah bobot saat ini lebih besar dari maks_bobot
+            if (bobot > maks_bobot) {
+                maks_bobot = bobot;
+                posisi_maks = {it1->first, it2->first};
+            }
+        }
+    }
+
+    // Menampilkan bobot maksimum dan posisi
+    cout << "Bobot Maksimum: " << maks_bobot << " pada posisi (" << posisi_maks.first << ", " << posisi_maks.second << ")\n";
+}
 
 //CODE
 }
 
 
 int main(){
+    // Contoh penggunaan fungsi bobot_maks
+    truk[1][2] = 10;
+    truk[2][3] = 15;
+    truk[3][4] = 20;
+
+    bobot_maks(truk);
 
 return 0;
 }
